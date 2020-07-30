05142e9204bb03cdf27b99100b968aace92151a1
ebgao <xb_gw@hotmail.com>
2020-07-30 22:08:05 +0800

    syl python1.1-first program

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print('Hello world!')
+print('What is your name?') #ask for their names
+myName = input()
+print('It is good to meet you ,' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
